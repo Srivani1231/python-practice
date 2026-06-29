b6fd373cda40a00ca45060500deb38976fa5022b
Srivani1231 <dasojusrivani@gmail.com>
2026-06-28 23:55:30 -0700

    Day9-Numpy Basics

---
+++ day9/array_info.py
@@ -0,0 +1,5 @@
+import numpy as np
+arr=np.array([10,20,30,40,50])
+print("size=",arr.size)
+print("dimensions=",arr.ndim)
+print("data type=",arr.dtype)
---
+++ day9/array_sort.py
@@ -0,0 +1,3 @@
+import numpy as np
+arr=np.array([1,4,2,5,3,6])
+print(np.sort(arr))
