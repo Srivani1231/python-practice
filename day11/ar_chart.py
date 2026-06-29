379a2b055b5ad21c107ab92f6eb23ba1c38dd399
Srivani1231 <dasojusrivani@gmail.com>
2026-06-28 23:57:22 -0700

    Day11-Matplotlib

---
+++ day11/ar_chart.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+students=["A","B","C","D"]
+marks=[70,80,90,20]
+plt.bar(students,marks)
+plt.show()
---
+++ day11/pie_chart.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+subjects=["python","sql","ml"]
+hours=[40,50,50]
+plt.pie(hours,labels=subjects)
+plt.show()
